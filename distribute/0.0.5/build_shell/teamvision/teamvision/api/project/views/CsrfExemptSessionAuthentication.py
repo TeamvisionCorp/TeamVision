9a566826b414750ebf2377c65a310eb29b2894c7
iquality <155174391@qq.com>
2019-08-30 22:48:35 +0800

    release 0.0.5

--- distribute/0.0.5/build_shell/teamvision/teamvision/api/project/views/CsrfExemptSessionAuthentication.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-
-class CsrfExemptSessionAuthentication (SessionAuthentication):
-
-    def enforce_csrf(self, request):
-        if request.path.startswith('/api'):
-            return
-        else:
-            return SessionAuthentication.enforce_csrf(CsrfExemptSessionAuthentication,request)
